51bc529bffaa98db412766216fbf46ac82388744
Andrei <46084496+andrei-bil@users.noreply.github.com>
2019-01-24 21:34:59 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     'database' => [
-        'name' => 'the_vault_landingpages',
+        'name' => 'db_name',
         'username' => 'root',
         'password' => '',
         'connection' => 'mysql:host=127.0.0.1',
